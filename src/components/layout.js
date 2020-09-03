37c8a0d208ff426f275c23991e16057b7c41f968
Sergio Can <gersiopumas@gmail.com>
2020-09-03 18:20:04 -0500

    Adding reactstrap

---
+++ src/components/layout.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// layout with global css
+export default function Layout({ children }) {
+  return <div>{children}</div>
+}
